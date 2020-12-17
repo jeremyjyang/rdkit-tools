17db7f8c6b0028063d863775cb3e3bc71173c795
Jeremy Yang <jeremyjyang@gmail.com>
2020-12-17 16:42:37 -0700

    Ok.

--- python/rdk_fp.py
+++ python/rdk_fp.py
@@ -4,6 +4,25 @@ https://www.rdkit.org/docs/source/
 https://www.rdkit.org/docs/GettingStartedInPython.html#morgan-fingerprints-circular-fingerprints
 
 """
+###
+## RDKFingerprint # returns ExplicitBitVect
+## args: minPath=1,maxPath=7,fpSize=2048,nBitsPerHash=4,useHs=True, tgtDensity=0.0,minSize=128
+##
+## GenMACCSKeys (no args) # returns SparseBitVect
+##
+## AllChem.GetMorganFingerprint(mol,2) # returns SparseBitVect
+## AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024)
+##
+## When comparing the ECFP/FCFP fingerprints and Morgan fingerprints by
+## RDKit, remember that 4 in ECFP4 corresponds to diameter of atom environments,
+## while Morgan fingerprints take a radius parameter. So radius=2 roughly
+## equivalent to ECFP4 and FCFP4.
+#rdkit.DataStructs.BitVectToText
+#rdkit.DataStructs.BitVectToBinaryText
+#rdkit.DataStructs.BitVectToFPSText
+#rdkit.DataStructs.CreateFromBinaryText
+#rdkit.DataStructs.CreateFromBitString
+#rdkit.DataStructs.CreateFromFPSText
 #############################################################################
 import os,sys,re,json,time,inspect,argparse,logging
 import matplotlib as mpl
@@ -14,10 +33,9 @@ import rdkit
 from rdkit.Chem import SmilesMolSupplier, SDMolSupplier, SDWriter, SmilesWriter, MolStandardize, MolToSmiles, MolFromSmiles, Draw, rdDepictor
 from rdkit.Chem import RDKFingerprint, PatternFingerprint, LayeredFingerprint, LayeredFingerprint_substructLayers
 from rdkit.Chem.MACCSkeys import GenMACCSKeys
-from rdkit.Chem.AllChem import GetMorganFingerprint
+from rdkit.Chem.AllChem import GetMorganFingerprint,GetMorganFingerprintAsBitVect
 
 from rdkit.Chem.Draw import rdMolDraw2D
-
 
 DEMOSMIS = [
 'C[C@H]1CN(CCCN1[S+]([O-])(=O)C2=CC=CC3=C2C(=CN=C3)C)C(=O)CN Rho Kinase Inhibitor IV',
@@ -73,54 +91,82 @@ def File2Molwriter(ofile, odelim, oheader):
   return molWriter
 
 #############################################################################
-## RDKFingerprint # returns ExplicitBitVect
-## args: minPath=1,maxPath=7,fpSize=2048,nBitsPerHash=4,useHs=True, tgtDensity=0.0,minSize=128
-##
-## GenMACCSKeys (no args) # returns SparseBitVect
-##
-## AllChem.GetMorganFingerprint(mol,2) # returns SparseBitVect
-## AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=1024)
-##
-## When comparing the ECFP/FCFP fingerprints and Morgan fingerprints by
-## RDKit, remember that 4 in ECFP4 corresponds to diameter of atom environments,
-## while Morgan fingerprints take a radius parameter. So radius=2 roughly
-## equivalent to ECFP4 and FCFP4.
-#############################################################################
-def Mols2FPs(mols, fper, molWriter):
-  #rdkit.DataStructs.CreateFromBinaryText
-  #rdkit.DataStructs.CreateFromFPSText
-  #rdkit.DataStructs.CreateFromBitString
-
+def Mols2FPs_RDK(mols, molWriter=None):
   for molA in mols:
-    fpA = fper(molA)
-    #logging.debug(rdkit.DataStructs.BitVectToText(fpA))
-    #logging.debug(rdkit.DataStructs.BitVectToBinaryText(fpA))
-    logging.debug(rdkit.DataStructs.BitVectToFPSText(fpA))
-    
+    fpA = RDKFingerprint(molA)
+    fpstxt = rdkit.DataStructs.BitVectToFPSText(fpA)
+    logging.debug(fpstxt)
+    molA.SetProp("_Name", (molA.GetProp("_Name") if molA.HasProp("_Name") else "")+"\t"+fpstxt)
     count=0;
     for i in range(fpA.GetNumBits()):
       if fpA.GetBit(i): count+=1
     logging.debug(f"count={count}; size={fpA.GetNumBits()}")
-    if fper==GenMACCSKeys:
-      onbits = tuple(fpA.GetOnBits())
-      logging.debug(str(onbits))
-      for i in onbits:
-        logging.debug(rdkit.Chem.MACCSkeys.smartsPatts[i][0])
+    if molWriter is not None:
+      molWriter.write(molA)
 
 #############################################################################
-def Demo(fper):
-  logging.debug(str(fper))
+def Mols2FPs_Morgan(mols, radius=2, nbits=1024, molWriter=None):
+  for molA in mols:
+    fpA = GetMorganFingerprintAsBitVect(molA, radius, nbits)
+    fpstxt = rdkit.DataStructs.BitVectToFPSText(fpA)
+    logging.debug(fpstxt)
+    molA.SetProp("_Name", (molA.GetProp("_Name") if molA.HasProp("_Name") else "")+"\t"+fpstxt)
+    count=0;
+    for i in range(fpA.GetNumBits()):
+      if fpA.GetBit(i): count+=1
+    logging.debug(f"count={count}; size={fpA.GetNumBits()}")
+    if molWriter is not None:
+      molWriter.write(molA)
+
+#############################################################################
+def Mols2FPs_MACCSKeys(mols, molWriter=None):
+  for molA in mols:
+    fpA = GenMACCSKeys(molA)
+    fpstxt = rdkit.DataStructs.BitVectToFPSText(fpA)
+    logging.debug(fpstxt)
+    molA.SetProp("_Name", (molA.GetProp("_Name") if molA.HasProp("_Name") else "")+"\t"+fpstxt)
+    count=0;
+    for i in range(fpA.GetNumBits()):
+      if fpA.GetBit(i): count+=1
+    logging.debug(f"count={count}; size={fpA.GetNumBits()}")
+    onbits = tuple(fpA.GetOnBits())
+    smarts = {i:rdkit.Chem.MACCSkeys.smartsPatts[i][0] for i in onbits}
+    logging.debug(str(smarts))
+    if molWriter is not None:
+      molWriter.write(molA)
+
+#############################################################################
+def Demo():
   mols=[];
   for smi in DEMOSMIS:
     mol = MolFromSmiles(re.sub(r'\s.*$', '', smi))
     mols.append(mol)
   molWriter = SmilesWriter("-", delimiter="\t", nameHeader='Name', includeHeader=True, isomericSmiles=True, kekuleSmiles=False)
-  Mols2FPs(mols, fper, molWriter)
+  Mols2FPs_RDK(mols, molWriter)
+ 
+#############################################################################
+def DemoMorgan():
+  mols=[];
+  for smi in DEMOSMIS:
+    mol = MolFromSmiles(re.sub(r'\s.*$', '', smi))
+    mols.append(mol)
+  molWriter = SmilesWriter("-", delimiter="\t", nameHeader='Name', includeHeader=True, isomericSmiles=True, kekuleSmiles=False)
+  Mols2FPs_Morgan(mols, 2, 1024, molWriter)
+ 
+#############################################################################
+def DemoMACCSKeys():
+  mols=[];
+  for smi in DEMOSMIS:
+    mol = MolFromSmiles(re.sub(r'\s.*$', '', smi))
+    mols.append(mol)
+  molWriter = SmilesWriter("-", delimiter="\t", nameHeader='Name', includeHeader=True, isomericSmiles=True, kekuleSmiles=False)
+  Mols2FPs_MACCSKeys(mols, molWriter)
  
 #############################################################################
 if __name__ == "__main__":
+  MORGAN_NBITS=1024; MORGAN_RADIUS=2;
   parser = argparse.ArgumentParser(description="RDKit fingerprint generator", epilog="")
-  OPS = ["demo", "fpgen"]
+  OPS = ["demo", "demo_maccs", "demo_morgan", "fpgen", "fpgen_morgan", "fpgen_maccs"]
   parser.add_argument("op", choices=OPS, help="OPERATION")
   parser.add_argument("--i", dest="ifile", help="input file, TSV or SDF")
   parser.add_argument("--o", dest="ofile", help="output file, TSV|SDF")
@@ -131,6 +177,8 @@ if __name__ == "__main__":
   parser.add_argument("--odelim", default="\t", help="delim for output TSV")
   parser.add_argument("--iheader", action="store_true", help="input TSV has header")
   parser.add_argument("--oheader", action="store_true", help="output TSV has header")
+  parser.add_argument("--morgan_nbits", type=int, default=MORGAN_NBITS)
+  parser.add_argument("--morgan_radius", type=int, default=MORGAN_RADIUS)
   parser.add_argument("-v", "--verbose", action="count", default=0)
 
   args = parser.parse_args()
@@ -142,22 +190,32 @@ if __name__ == "__main__":
 
   t0=time.time()
 
-  fpers = [RDKFingerprint, GenMACCSKeys, GetMorganFingerprint]
-  #fper = RDKFingerprint
-  fper = GenMACCSKeys
-  #fper = GetMorganFingerprint
-
   if args.op=="demo":
-    Demo(fper)
+    Demo()
+    sys.exit()
+
+  elif args.op=="demo_morgan":
+    DemoMorgan()
+    sys.exit()
+
+  elif args.op=="demo_maccs":
+    DemoMACCSKeys()
     sys.exit()
 
   if not (args.ifile): parser.error('--i required.')
 
+  molReader = File2Molreader(args.ifile, args.idelim, args.smicol, args.namcol, args.iheader)
+  molWriter = File2Molwriter(args.ofile, args.odelim, args.oheader)
+  mols = ReadMols(molReader)
+
   if args.op=="fpgen":
-    molReader = File2Molreader(args.ifile, args.idelim, args.smicol, args.namcol, args.iheader)
-    molWriter = File2Molwriter(args.ofile, args.odelim, args.oheader)
-    mols = ReadMols(molReader)
-    Mols2FPs(mols, fper, molWriter)
+    Mols2FPs_RDK(mols, molWriter)
+
+  elif args.op=="fpgen_morgan":
+    Mols2FPs_Morgan(mols, args.morgan_radius, args.morgan_nbits, molWriter)
+
+  elif args.op=="fpgen_maccs":
+    Mols2FPs_MACCS(mols, molWriter)
 
   else:
     parser.error(f"Unsupported operation: {args.op}")
