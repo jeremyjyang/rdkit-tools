897e64552291dec7e3bcc9f9a886236c6b29a09a
Jeremy Yang <jeremyjyang@gmail.com>
2021-06-05 13:30:42 -0600

    Ok.

--- README.md
+++ README.md
@@ -7,8 +7,37 @@ Tools for use with RDKit.
 
 ## Dependencies
 
-* RDKit installed.
-* `RDBASE` = root directory of the RDKit distribution
-* `PYTHONPATH` includes `$RDBASE`.
-* Linux: `LD_LIBRARY_PATH`: includes `$RDBASE/lib` and Boost shared libraries 
-* OS X: `DYLD_LIBRARY_PATH`: includes `$RDBASE/lib` and Boost shared libraries 
+* RDKit Python package (possibly via conda).
+
+## Execution
+
+```
+$ conda activate
+(base) $ python3 -m rdkwrapper.scaffold.App -h
+usage: App.py [-h] [--i IFILE] [--o OFILE] [--o_html OFILE_HTML]
+              [--scratchdir SCRATCHDIR] [--smicol SMICOL] [--namcol NAMCOL]
+              [--idelim IDELIM] [--odelim ODELIM] [--iheader] [--oheader]
+              [--brics] [-v]
+              {bmscaf,scafnet,demobm,demonet,demonetvis}
+
+RDKit scaffold analysis
+
+positional arguments:
+  {bmscaf,scafnet,demobm,demonet,demonetvis}
+                        OPERATION
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --i IFILE             input file, TSV or SDF
+  --o OFILE             output file, TSV|SDF
+  --o_html OFILE_HTML   output file, HTML
+  --scratchdir SCRATCHDIR
+  --smicol SMICOL       SMILES column from TSV (counting from 0)
+  --namcol NAMCOL       name column from TSV (counting from 0)
+  --idelim IDELIM       delim for input TSV
+  --odelim ODELIM       delim for output TSV
+  --iheader             input TSV has header
+  --oheader             output TSV has header
+  --brics               BRICS fragmentation rules (Degen, 2008)
+  -v, --verbose
+```
--- rdkwrapper/scaffold/App.py
+++ rdkwrapper/scaffold/App.py
@@ -31,31 +31,52 @@ def DemoBM():
   img.show()
 
 #############################################################################
-def DemoNet(brics):
+def DemoNetImg(brics, ofile):
+  logging.debug(f"DemoNetImg({brics}, {ofile})")
   smi = ('Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin')
   mols = [MolFromSmiles(re.sub(r'\s.*$', '', smi))]
   scafnet = scaffold.Utils.Mols2ScafNet(mols, brics)
-  scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
+  logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
+  #scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
+  scafmols = []
+  for i,m in enumerate(scafnet.nodes[:]):
+    logging.debug(f"{i+1}. MolFromSmiles({m})...")
+    scafmols.append(MolFromSmiles(m))
+
+  logging.info(f"Scafmols: {len(scafmols)}")
   #title="RDKit_ScafNet:"+re.sub(r'^[^\s]*\s+(.*)$', r'\1', smi)) #How to add title?
   img = rdkit.Chem.Draw.MolsToGridImage(scafmols, legends=[f'{i}, counts: {c}' for i,c in enumerate(scafnet.counts)], molsPerRow=4)
+  logging.debug(f"img.save({ofile})...")
+  img.save(ofile, format="PNG")
+  logging.debug(f"img.save({ofile})...Done.")
   img.show()
 
 #############################################################################
-def DemoNetVis(brics, scratchdir, ofile):
+def DemoNetHtml(brics, scratchdir, ofile):
+  logging.debug(f"DemoNetHtml({brics}, {scratchdir}, {ofile})")
   DEMOSMI = ('Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin')
   mols = [MolFromSmiles(re.sub(r'\s.*$', '', DEMOSMI))]
   scafnet = scaffold.Utils.Mols2ScafNet(mols, brics)
+  logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
   if not os.path.isdir(scratchdir): os.mkdir(scratchdir)
-    
+
+  logging.debug(f"pyvis.network.Network()...")
   g = pyvis.network.Network(notebook=False, height='800px', width='1000px', heading="RDKit_ScafNet:"+re.sub(r'^[^\s]*\s+(.*)$', r'\1', DEMOSMI))
+  logging.debug(f"pyvis.network.Network()... Done.")
 
   for i,n in enumerate(scafnet.nodes):
+    logging.debug(f"{i+1}. util.Utils.moltosvg(rdkit.Chem.MolFromSmiles({n})...")
     svg = util.Utils.moltosvg(rdkit.Chem.MolFromSmiles(n))
     with open(f'{scratchdir}/{i}.svg', 'w') as outf:
       outf.write(svg)
+    logging.debug(f"g.add_node()...")
     g.add_node(i, shape="image", label=' ', image=f'{scratchdir}/{i}.svg', title=svg, size=60)
-  for e in scafnet.edges:
+    # Segmentation fault after last node.
+  logging.debug(f"util.Utils.moltosvg()... Done.")
+  for i,e in enumerate(scafnet.edges):
+    logging.debug(f"{i+1}. g.add_edge()...")
     g.add_edge(e.beginIdx, e.endIdx, label=str(e.type))
+  logging.debug(f"g.add_edge()... Done.")
 
   VisJS_options = {
     "edges": {
@@ -80,19 +101,21 @@ def DemoNetVis(brics, scratchdir, ofile):
       "solver": "forceAtlas2Based"
     }
   }
+  logging.debug(f"g.set_options()...")
   g.set_options(options=json.dumps(VisJS_options))
   #g.show_buttons()
   logging.info(f"Writing SCAFNET to: {ofile}")
+  logging.debug(f"g.show()...")
   g.show(ofile)
 
 #############################################################################
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="RDKit scaffold analysis", epilog="")
-  OPS = ["bmscaf", "scafnet", "demobm", "demonet", "demonetvis"]
+  OPS = ["bmscaf", "scafnet", "demobm", "demonet_img", "demonet_html"]
   parser.add_argument("op", choices=OPS, default="mol2scaf", help="OPERATION")
   parser.add_argument("--i", dest="ifile", help="input file, TSV or SDF")
   parser.add_argument("--o", dest="ofile", help="output file, TSV|SDF")
-  parser.add_argument("--o_html", dest="ofile_html", default="/tmp/rdk_scafnet.html", help="output file, HTML")
+  parser.add_argument("--o_vis", dest="ofile_vis", default="/tmp/rdk_scafnet.html", help="output file, PNG or HTML")
   parser.add_argument("--scratchdir", default="/tmp")
   parser.add_argument("--smicol", type=int, default=0, help="SMILES column from TSV (counting from 0)")
   parser.add_argument("--namcol", type=int, default=1, help="name column from TSV (counting from 0)")
@@ -102,7 +125,6 @@ if __name__ == "__main__":
   parser.add_argument("--oheader", action="store_true", help="output TSV has header")
   parser.add_argument("--brics", action="store_true", help="BRICS fragmentation rules (Degen, 2008)")
   parser.add_argument("-v", "--verbose", action="count", default=0)
-
   args = parser.parse_args()
 
   logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
@@ -117,12 +139,12 @@ if __name__ == "__main__":
     DemoBM()
     sys.exit()
 
-  elif args.op=="demonet":
-    DemoNet(args.brics)
+  elif args.op=="demonet_img":
+    DemoNetImg(args.brics, args.ofile_vis)
     sys.exit()
 
-  elif args.op=="demonetvis":
-    DemoNetVis(args.brics, args.scratchdir, args.ofile_html)
+  elif args.op=="demonet_html":
+    DemoNetHtml(args.brics, args.scratchdir, args.ofile_vis)
     sys.exit()
 
   if not (args.ifile): parser.error('--i required.')
