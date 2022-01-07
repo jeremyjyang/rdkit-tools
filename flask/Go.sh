ebb04926908c84a4edb7a574efee4b5ac3babaa2
Jeremy Yang <jeremyjyang@gmail.com>
2022-01-07 11:34:18 -0700

    Alright.

--- flask/Go.sh
+++ flask/Go.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 ###
-# https://flask.palletsprojects.com/en/2.0.x/installation/
-# python3 -m venv venv
-# . venv/bin/activate
-# pip install Flask
-###
 # https://flask.palletsprojects.com/en/2.0.x/tutorial/blog/
 ###
-# VirtualEnv should be activated:
-# . venv/bin/activate
+# We need to use Conda, not VirtualEnv, for RDKit support.
+# https://softwarejargon.com/dockerizing-python-flask-app-and-conda-environment/
 ###
+# conda create -c conda-forge -n rdktools rdkit
+# conda activate rdktools
+# pip install Flask
+# Or:
+# conda env create -f environment.yml
+# conda activate rdktools
+###
+source $(dirname $(which conda))/../bin/activate rdktools
 export FLASK_APP=depict
 export FLASK_ENV=development
-#flask init-db
 flask run
+conda deactivate
 ###
-# VirtualEnv deactivate:
-# deactivate
--- flask/depict/__init__.py
+++ flask/depict/__init__.py
@@ -1,9 +1,19 @@
 # Based on https://flask.palletsprojects.com/en/2.0.x/tutorial/factory/
 
-import os
+import os,io,base64,logging
 
-from flask import Flask
+from flask import Flask,render_template
+from markupsafe import escape
 
+import rdkit.Chem
+import rdkit.Chem.Draw
+import rdkit.Chem.AllChem
+#import collections
+#from rdkit.Chem.Draw.MolDrawing import DrawingOptions
+
+from rdktools import depict
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 def create_app(test_config=None):
     # create and configure the app
@@ -26,13 +36,36 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    # simple hello
+    #@app.route('/hello')
+    #def hello():
+    #    return 'Hello, World!'
+
+    # better hello
+    @app.route('/hello/')
+    @app.route('/hello/<name>')
+    def hello(name=None):
+      return render_template('hello.html', name=name)
 
     @app.route('/')
     def home():
         return 'Depict app (under construction).'
+
+    @app.route('/mol2img/<smiles>', methods=['GET'])
+    def mol2img(smiles):
+      # show depiction for smiles
+      mol = rdkit.Chem.MolFromSmiles(smiles)
+      #mol = rdkit.Chem.AddHs(mol)
+      rdkit.Chem.AllChem.Compute2DCoords(mol, clearConfs=True)
+      # PIL.Image.Image
+      img = depict.Utils.Mol2Image(mol, width=300, height=200, kekulize=True, wedgeBonds=True)
+      img_bytearray = io.BytesIO()
+      #img_bytearray.write(b'Content-type: image/png\n\n')
+      img.save(img_bytearray, format='PNG')
+      imgdata_b64 = base64.b64encode(img_bytearray.getvalue())
+      logging.debug(f"SMILES: {escape(smiles)}")
+      logging.debug(f"len(imgdata_b64): {len(imgdata_b64)}")
+      return render_template('image.html', result=imgdata_b64)
+
 
     return app
