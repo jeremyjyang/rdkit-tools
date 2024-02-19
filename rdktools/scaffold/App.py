e57c2682535c8acbc5310720b3fcd14aa97f3986
Jack-42 <johnjack0987@gmail.com>
2024-02-19 09:47:54 -0700

    remove unused imports, apply black formatting

--- rdktools/scaffold/App.py
+++ rdktools/scaffold/App.py
@@ -4,101 +4,150 @@ https://www.rdkit.org/docs/source/rdkit.Chem.Scaffolds.MurckoScaffold.html
 https://www.rdkit.org/docs/source/rdkit.Chem.Scaffolds.rdScaffoldNetwork.html
 rdScaffoldNetwork available RDKit 2020.03.1+.
 """
+import argparse
+import logging
+
 #############################################################################
-import os,sys,stat,re,json,time,inspect,argparse,logging
+import os
+import sys
+import time
+
 import matplotlib as mpl
 import pyvis
-from pyvis.network import Network
-
 import rdkit
-from rdkit.Chem import SmilesMolSupplier, SDMolSupplier, SDWriter, SmilesWriter, MolStandardize, MolToSmiles, MolFromSmiles, Draw
-from rdkit.Chem.Scaffolds import MurckoScaffold
-from rdkit.Chem.Scaffolds import rdScaffoldNetwork
 
-from .. import scaffold
-from .. import util
+from .. import scaffold, util
 
 SCRATCHDIR = f"{os.environ['HOME']}/tmp/rdktools"
 
 #############################################################################
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="RDKit scaffold analysis", epilog="")
-  OPS = ["bmscaf", "scafnet", "scafnet_rings", "demobm", "demonet_img", "demonet_html"]
-  parser.add_argument("op", choices=OPS, default="mol2scaf", help="OPERATION")
-  parser.add_argument("--i", dest="ifile", help="input file, TSV or SDF")
-  parser.add_argument("--o", dest="ofile", help="output file, TSV|SDF")
-  parser.add_argument("--scratchdir", default=SCRATCHDIR)
-  parser.add_argument("--o_png", dest="ofile_png", default=f"{SCRATCHDIR}/rdktools_scafnet.png", help="visualization output file, PNG")
-  parser.add_argument("--o_html", dest="ofile_html", default=f"{SCRATCHDIR}/rdktools_scafnet.html", help="visualization output file, HTML")
-  parser.add_argument("--smilesColumn", type=int, default=0, help="SMILES column from TSV (counting from 0)")
-  parser.add_argument("--nameColumn", type=int, default=1, help="name column from TSV (counting from 0)")
-  parser.add_argument("--idelim", default="\t", help="delim for input TSV")
-  parser.add_argument("--odelim", default="\t", help="delim for output TSV")
-  parser.add_argument("--iheader", action="store_true", help="input TSV has header")
-  parser.add_argument("--oheader", action="store_true", help="output TSV has header")
-  parser.add_argument("--brics", action="store_true", help="BRICS fragmentation rules (Degen, 2008)")
-  parser.add_argument("--scafname", default="RDKit Scaffold Analysis", help="title for output")
-  parser.add_argument("--display", action="store_true", help="Display scafnet interactively.")
-  parser.add_argument("-v", "--verbose", action="count", default=0)
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="RDKit scaffold analysis", epilog="")
+    OPS = [
+        "bmscaf",
+        "scafnet",
+        "scafnet_rings",
+        "demobm",
+        "demonet_img",
+        "demonet_html",
+    ]
+    parser.add_argument("op", choices=OPS, default="mol2scaf", help="OPERATION")
+    parser.add_argument("--i", dest="ifile", help="input file, TSV or SDF")
+    parser.add_argument("--o", dest="ofile", help="output file, TSV|SDF")
+    parser.add_argument("--scratchdir", default=SCRATCHDIR)
+    parser.add_argument(
+        "--o_png",
+        dest="ofile_png",
+        default=f"{SCRATCHDIR}/rdktools_scafnet.png",
+        help="visualization output file, PNG",
+    )
+    parser.add_argument(
+        "--o_html",
+        dest="ofile_html",
+        default=f"{SCRATCHDIR}/rdktools_scafnet.html",
+        help="visualization output file, HTML",
+    )
+    parser.add_argument(
+        "--smilesColumn",
+        type=int,
+        default=0,
+        help="SMILES column from TSV (counting from 0)",
+    )
+    parser.add_argument(
+        "--nameColumn",
+        type=int,
+        default=1,
+        help="name column from TSV (counting from 0)",
+    )
+    parser.add_argument("--idelim", default="\t", help="delim for input TSV")
+    parser.add_argument("--odelim", default="\t", help="delim for output TSV")
+    parser.add_argument("--iheader", action="store_true", help="input TSV has header")
+    parser.add_argument("--oheader", action="store_true", help="output TSV has header")
+    parser.add_argument(
+        "--brics", action="store_true", help="BRICS fragmentation rules (Degen, 2008)"
+    )
+    parser.add_argument(
+        "--scafname", default="RDKit Scaffold Analysis", help="title for output"
+    )
+    parser.add_argument(
+        "--display", action="store_true", help="Display scafnet interactively."
+    )
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    args = parser.parse_args()
 
-  logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
+    logging.basicConfig(
+        format="%(levelname)s:%(message)s",
+        level=(logging.DEBUG if args.verbose > 1 else logging.INFO),
+    )
 
-  logging.info(f"RDKit version: {rdkit.rdBase.rdkitVersion}")
-  logging.info(f"MatplotLib version: {mpl.__version__}")
-  logging.info(f"Pyvis version: {pyvis.__version__}")
+    logging.info(f"RDKit version: {rdkit.rdBase.rdkitVersion}")
+    logging.info(f"MatplotLib version: {mpl.__version__}")
+    logging.info(f"Pyvis version: {pyvis.__version__}")
 
-  t0=time.time()
+    t0 = time.time()
 
-  if not os.path.isdir(args.scratchdir): os.mkdir(args.scratchdir)
+    if not os.path.isdir(args.scratchdir):
+        os.mkdir(args.scratchdir)
 
-  if args.op=="demobm":
-    scaffold.DemoBM()
-    sys.exit()
+    if args.op == "demobm":
+        scaffold.DemoBM()
+        sys.exit()
 
-  elif args.op=="demonet_img":
-    scaffold.DemoNetImg(args.scratchdir)
-    sys.exit()
+    elif args.op == "demonet_img":
+        scaffold.DemoNetImg(args.scratchdir)
+        sys.exit()
 
-  elif args.op=="demonet_html":
-    logging.debug(f"scratchdir: {args.scratchdir}")
-    scaffold.DemoNetHtml(args.scratchdir)
-    sys.exit()
+    elif args.op == "demonet_html":
+        logging.debug(f"scratchdir: {args.scratchdir}")
+        scaffold.DemoNetHtml(args.scratchdir)
+        sys.exit()
 
-  if not (args.ifile): parser.error('--i required.')
+    if not (args.ifile):
+        parser.error("--i required.")
 
-  if args.op=="bmscaf":
-    molReader = util.File2Molreader(args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader)
-    molWriter = util.File2Molwriter(args.ofile, args.odelim, args.oheader)
-    mols = util.ReadMols(molReader)
-    scafmols = scaffold.Mols2BMScaffolds(mols, molWriter)
-    if args.ofile_png:
-      img = rdkit.Chem.Draw.MolsToGridImage(scafmols, molsPerRow=8)
-      img.save(args.ofile_png, format="PNG")
+    if args.op == "bmscaf":
+        molReader = util.File2Molreader(
+            args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader
+        )
+        molWriter = util.File2Molwriter(args.ofile, args.odelim, args.oheader)
+        mols = util.ReadMols(molReader)
+        scafmols = scaffold.Mols2BMScaffolds(mols, molWriter)
+        if args.ofile_png:
+            img = rdkit.Chem.Draw.MolsToGridImage(scafmols, molsPerRow=8)
+            img.save(args.ofile_png, format="PNG")
 
-  elif args.op=="scafnet":
-    molReader = util.File2Molreader(args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader)
-    fout = open(args.ofile, "w") if ofile else sys.stdout
-    mols = util.ReadMols(molReader)
-    scafnet = scaffold.Mols2ScafNet(mols, args.brics, fout)
-    if args.ofile_png:
-      scaffold.Scafnet2Img(scafnet, args.ofile_png)
-    if args.ofile_html:
-      scaffold.Scafnet2Html(scafnet, args.scafname, args.scratchdir, args.ofile_html)
+    elif args.op == "scafnet":
+        molReader = util.File2Molreader(
+            args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader
+        )
+        fout = open(args.ofile, "w") if ofile else sys.stdout
+        mols = util.ReadMols(molReader)
+        scafnet = scaffold.Mols2ScafNet(mols, args.brics, fout)
+        if args.ofile_png:
+            scaffold.Scafnet2Img(scafnet, args.ofile_png)
+        if args.ofile_html:
+            scaffold.Scafnet2Html(
+                scafnet, args.scafname, args.scratchdir, args.ofile_html
+            )
 
-  elif args.op=="scafnet_rings":
-    molReader = util.File2Molreader(args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader)
-    molWriter = util.File2Molwriter(args.ofile, args.odelim, args.oheader)
-    n_mol=0;
-    for mol in molReader:
-      n_mol+=1
-      molname = mol.GetProp('_Name') if mol.HasProp('_Name') else f"Mol_{n_mol:03d}"
-      scafnet = scaffold.Mols2ScafNet([mol], args.brics, None)
-      rings = scaffold.ScafNet2Rings(scafnet, molname, molWriter)
-    logging.info(f"Mols processed: {n_mol}")
+    elif args.op == "scafnet_rings":
+        molReader = util.File2Molreader(
+            args.ifile, args.idelim, args.smilesColumn, args.nameColumn, args.iheader
+        )
+        molWriter = util.File2Molwriter(args.ofile, args.odelim, args.oheader)
+        n_mol = 0
+        for mol in molReader:
+            n_mol += 1
+            molname = (
+                mol.GetProp("_Name") if mol.HasProp("_Name") else f"Mol_{n_mol:03d}"
+            )
+            scafnet = scaffold.Mols2ScafNet([mol], args.brics, None)
+            rings = scaffold.ScafNet2Rings(scafnet, molname, molWriter)
+        logging.info(f"Mols processed: {n_mol}")
 
-  else:
-    parser.error(f"Unsupported operation: {args.op}")
+    else:
+        parser.error(f"Unsupported operation: {args.op}")
 
-  logging.info(f"""Elapsed: {time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))}""")
-
+    logging.info(
+        f"""Elapsed: {time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))}"""
+    )
--- rdktools/scaffold/Utils.py
+++ rdktools/scaffold/Utils.py
@@ -7,203 +7,236 @@ rdScaffoldNetwork available RDKit 2020.03.1+.
 https://matplotlib.org/
 https://pyvis.readthedocs.io/en/latest/
 """
+import inspect
+import json
+import logging
+
 #############################################################################
-import os,sys,re,logging,json,time,inspect,tempfile,stat
+import os
+import re
+import stat
+import tempfile
 
-import matplotlib as mpl
-#from matplotlib import pyplot as plt
-
+import pyvis
 import rdkit
 import rdkit.Chem
 import rdkit.Chem.AllChem
-from rdkit.Chem import SmilesMolSupplier, SDMolSupplier, SDWriter, SmilesWriter, MolStandardize, MolToSmiles, MolFromSmiles, Draw, rdDepictor
-from rdkit.Chem.AllChem import Compute2DCoords
 
 # fingerprints:
-from rdkit.Chem import RDKFingerprint, PatternFingerprint, LayeredFingerprint, LayeredFingerprint_substructLayers
-from rdkit.Chem.MACCSkeys import GenMACCSKeys
-from rdkit.Chem.AllChem import GetMorganFingerprint,GetMorganFingerprintAsBitVect
+from rdkit.Chem import MolFromSmiles, MolToSmiles
+from rdkit.Chem.AllChem import Compute2DCoords
 
 # scaffolds:
-from rdkit.Chem.Scaffolds import MurckoScaffold
-from rdkit.Chem.Scaffolds import rdScaffoldNetwork
-
-import pyvis
-from pyvis.network import Network
+from rdkit.Chem.Scaffolds import MurckoScaffold, rdScaffoldNetwork
 
 from .. import util
 
+# from matplotlib import pyplot as plt
+
+
 #############################################################################
 def Mols2BMScaffolds(mols, molWriter):
-  scafmols=[];
-  for i,mol in enumerate(mols):
-    molname = mol.GetProp('_Name') if mol.HasProp('_Name') else ''
-    logging.debug(f'{i+1}. {molname}')
-    scafmol = MurckoScaffold.GetScaffoldForMol(mol)
-    Compute2DCoords(scafmol, clearConfs=True)
-    scafmols.append(scafmol)
-    molWriter.write(scafmol)
-  logging.info(f'{len(mols)} mols written to {molWriter}')
-  return scafmols
+    scafmols = []
+    for i, mol in enumerate(mols):
+        molname = mol.GetProp("_Name") if mol.HasProp("_Name") else ""
+        logging.debug(f"{i+1}. {molname}")
+        scafmol = MurckoScaffold.GetScaffoldForMol(mol)
+        Compute2DCoords(scafmol, clearConfs=True)
+        scafmols.append(scafmol)
+        molWriter.write(scafmol)
+    logging.info(f"{len(mols)} mols written to {molWriter}")
+    return scafmols
+
 
 #############################################################################
 def Mols2ScafNet(mols, brics=False, fout=None):
-  if brics:
-    params = rdScaffoldNetwork.BRICSScaffoldParams()
-  else:
-    params = rdScaffoldNetwork.ScaffoldNetworkParams()
-    params.flattenChirality = True
-    params.flattenIsotopes = True
-    params.flattenKeepLargest = True
-    params.includeGenericBondScaffolds = False
-    params.includeGenericScaffolds = False
-    params.includeScaffoldsWithAttachments = False
-    params.includeScaffoldsWithoutAttachments = True
-    params.keepOnlyFirstFragment = False
-    params.pruneBeforeFragmenting = True
+    if brics:
+        params = rdScaffoldNetwork.BRICSScaffoldParams()
+    else:
+        params = rdScaffoldNetwork.ScaffoldNetworkParams()
+        params.flattenChirality = True
+        params.flattenIsotopes = True
+        params.flattenKeepLargest = True
+        params.includeGenericBondScaffolds = False
+        params.includeGenericScaffolds = False
+        params.includeScaffoldsWithAttachments = False
+        params.includeScaffoldsWithoutAttachments = True
+        params.keepOnlyFirstFragment = False
+        params.pruneBeforeFragmenting = True
 
-  attrs = [a for a in inspect.getmembers(params) if not(a[0].startswith('__'))]
-  for a in attrs:
-    logging.debug(f"{a[0]}: {a[1]}")
+    attrs = [a for a in inspect.getmembers(params) if not (a[0].startswith("__"))]
+    for a in attrs:
+        logging.debug(f"{a[0]}: {a[1]}")
 
-  for i,mol in enumerate(mols):
-    molname = mol.GetProp('_Name') if mol.HasProp('_Name') else ''
-    logging.debug(f'{i+1}. {molname}:')
+    for i, mol in enumerate(mols):
+        molname = mol.GetProp("_Name") if mol.HasProp("_Name") else ""
+        logging.debug(f"{i+1}. {molname}:")
 
-  scafnet = rdScaffoldNetwork.CreateScaffoldNetwork(mols, params)
-  if fout is not None:
-    for i in range(len(scafnet.nodes)):
-      fout.write(f"node\t{i}\t{scafnet.nodes[i]}\t{scafnet.counts[i]}\n")
-    for i in range(len(scafnet.edges)):
-      fout.write(f"edge\t{i}\t{scafnet.edges[i].beginIdx}\t{scafnet.edges[i].endIdx}\t{scafnet.edges[i].type}\n")
-    fout.flush()
-  logging.info(f"nodes: {len(scafnet.nodes)}; edges:{len(scafnet.edges)}")
-  return scafnet
+    scafnet = rdScaffoldNetwork.CreateScaffoldNetwork(mols, params)
+    if fout is not None:
+        for i in range(len(scafnet.nodes)):
+            fout.write(f"node\t{i}\t{scafnet.nodes[i]}\t{scafnet.counts[i]}\n")
+        for i in range(len(scafnet.edges)):
+            fout.write(
+                f"edge\t{i}\t{scafnet.edges[i].beginIdx}\t{scafnet.edges[i].endIdx}\t{scafnet.edges[i].type}\n"
+            )
+        fout.flush()
+    logging.info(f"nodes: {len(scafnet.nodes)}; edges:{len(scafnet.edges)}")
+    return scafnet
+
 
 #############################################################################
 def ScafNet2Rings(scafnet, name, molWriter):
-  """Output unique ringsystems only."""
-  ringsmis=set(); rings=[]
-  pat = rdkit.Chem.MolFromSmarts("*!@-*") #Non-ring single bond.
-  for smi in scafnet.nodes:
-    logging.debug(f"node: {smi}")
-    if smi not in ringsmis:
-      ring = MolFromSmiles(smi) #Ring-maybe.
-      if len(ring.GetSubstructMatches(pat))>0: continue
-      ringsmis.add(smi)
-      rings.append(ring)
-  rings_mol = MolFromSmiles(".".join(sorted(list(ringsmis))))
-  rings_mol.SetProp("_Name", f"{name}_RINGS")
-  molWriter.write(rings_mol)
-  logging.info(f"{name}: rings: {len(ringsmis)}")
-  return rings
+    """Output unique ringsystems only."""
+    ringsmis = set()
+    rings = []
+    pat = rdkit.Chem.MolFromSmarts("*!@-*")  # Non-ring single bond.
+    for smi in scafnet.nodes:
+        logging.debug(f"node: {smi}")
+        if smi not in ringsmis:
+            ring = MolFromSmiles(smi)  # Ring-maybe.
+            if len(ring.GetSubstructMatches(pat)) > 0:
+                continue
+            ringsmis.add(smi)
+            rings.append(ring)
+    rings_mol = MolFromSmiles(".".join(sorted(list(ringsmis))))
+    rings_mol.SetProp("_Name", f"{name}_RINGS")
+    molWriter.write(rings_mol)
+    logging.info(f"{name}: rings: {len(ringsmis)}")
+    return rings
+
 
 #############################################################################
 def DemoBM():
-  scafmols=[];
-  for smi in util.DEMOSMIS:
-    mol = MolFromSmiles(re.sub(r'\s.*$', '', smi))
-    scafmol = MurckoScaffold.GetScaffoldForMol(mol) if mol else None
-    scafmols.append(scafmol)
-    smi_std = MolToSmiles(scafmol, isomericSmiles=False) if scafmol else None
-    logging.info(f"{smi:>28s} >> {smi_std}")
-  img = rdkit.Chem.Draw.MolsToGridImage(scafmols, molsPerRow=4)
-  img.show()
+    scafmols = []
+    for smi in util.DEMOSMIS:
+        mol = MolFromSmiles(re.sub(r"\s.*$", "", smi))
+        scafmol = MurckoScaffold.GetScaffoldForMol(mol) if mol else None
+        scafmols.append(scafmol)
+        smi_std = MolToSmiles(scafmol, isomericSmiles=False) if scafmol else None
+        logging.info(f"{smi:>28s} >> {smi_std}")
+    img = rdkit.Chem.Draw.MolsToGridImage(scafmols, molsPerRow=4)
+    img.show()
+
 
 #############################################################################
 def DemoNetImg(scratchdir):
-  fout = tempfile.NamedTemporaryFile(prefix=scratchdir+"/", suffix=".png", mode="w+b", delete=False)
-  ofile = fout.name
-  fout.close()
-  brics=True
-  logging.debug(f"DemoNetImg({brics}, {fout.name})")
-  smi = ('Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin')
-  mols = [MolFromSmiles(re.sub(r'\s.*$', '', smi))]
-  scafnet = Mols2ScafNet(mols, False)
-  logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
-  #scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
-  scafmols = []
-  for i,m in enumerate(scafnet.nodes[:]):
-    logging.debug(f"{i+1}. MolFromSmiles({m})...")
-    scafmols.append(MolFromSmiles(m))
-  logging.info(f"Scafmols: {len(scafmols)}")
-  img = Scafnet2Img(scafnet, ofile)
-  img.show()
+    fout = tempfile.NamedTemporaryFile(
+        prefix=scratchdir + "/", suffix=".png", mode="w+b", delete=False
+    )
+    ofile = fout.name
+    fout.close()
+    brics = True
+    logging.debug(f"DemoNetImg({brics}, {fout.name})")
+    smi = "Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin"
+    mols = [MolFromSmiles(re.sub(r"\s.*$", "", smi))]
+    scafnet = Mols2ScafNet(mols, False)
+    logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
+    # scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
+    scafmols = []
+    for i, m in enumerate(scafnet.nodes[:]):
+        logging.debug(f"{i+1}. MolFromSmiles({m})...")
+        scafmols.append(MolFromSmiles(m))
+    logging.info(f"Scafmols: {len(scafmols)}")
+    img = Scafnet2Img(scafnet, ofile)
+    img.show()
+
 
 #############################################################################
 def Scafnet2Img(scafnet, ofile):
-  #title="RDKit_ScafNet:"+re.sub(r'^[^\s]*\s+(.*)$', r'\1', smi)) #How to add title?
-  scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
-  img = rdkit.Chem.Draw.MolsToGridImage(scafmols, legends=[f'{i}, counts: {c}' for i,c in enumerate(scafnet.counts)], molsPerRow=4)
-  logging.debug(f"Writing scafnet PNG to: {ofile}")
-  img.save(ofile, format="PNG")
-  return img
+    # title="RDKit_ScafNet:"+re.sub(r'^[^\s]*\s+(.*)$', r'\1', smi)) #How to add title?
+    scafmols = [MolFromSmiles(m) for m in scafnet.nodes]
+    img = rdkit.Chem.Draw.MolsToGridImage(
+        scafmols,
+        legends=[f"{i}, counts: {c}" for i, c in enumerate(scafnet.counts)],
+        molsPerRow=4,
+    )
+    logging.debug(f"Writing scafnet PNG to: {ofile}")
+    img.save(ofile, format="PNG")
+    return img
+
 
 #############################################################################
 def DemoNetHtml(scratchdir):
-  logging.debug(f"scratchdir: {scratchdir}")
-  fout = tempfile.NamedTemporaryFile(prefix=scratchdir+"/", suffix=".html", mode="w+", delete=False)
-  ofile = fout.name
-  logging.debug(f"ofile: {ofile}")
-  fout.close()
-  logging.debug(f"DemoNetHtml({scratchdir}, {ofile})")
-  demosmi = ('Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin')
-  mols = [MolFromSmiles(re.sub(r'\s.*$', '', demosmi))]
-  scafnet = Mols2ScafNet(mols, False)
-  logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
-  g = Scafnet2Html(scafnet, "RDKit_ScafNet: "+re.sub(r'^[^\s]*\s+(.*)$', r'\1', demosmi), scratchdir, ofile)
-  os.chmod(ofile, stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP|stat.S_IWGRP|stat.S_IROTH|stat.S_IWOTH)
-  g.show(ofile)
+    logging.debug(f"scratchdir: {scratchdir}")
+    fout = tempfile.NamedTemporaryFile(
+        prefix=scratchdir + "/", suffix=".html", mode="w+", delete=False
+    )
+    ofile = fout.name
+    logging.debug(f"ofile: {ofile}")
+    fout.close()
+    logging.debug(f"DemoNetHtml({scratchdir}, {ofile})")
+    demosmi = "Cc1onc(-c2c(F)cccc2Cl)c1C(=O)N[C@@H]1C(=O)N2[C@@H](C(=O)O)C(C)(C)S[C@H]12 flucloxacillin"
+    mols = [MolFromSmiles(re.sub(r"\s.*$", "", demosmi))]
+    scafnet = Mols2ScafNet(mols, False)
+    logging.info(f"Scafnet nodes: {len(scafnet.nodes)}; edges: {len(scafnet.edges)}")
+    g = Scafnet2Html(
+        scafnet,
+        "RDKit_ScafNet: " + re.sub(r"^[^\s]*\s+(.*)$", r"\1", demosmi),
+        scratchdir,
+        ofile,
+    )
+    os.chmod(
+        ofile,
+        stat.S_IRUSR
+        | stat.S_IWUSR
+        | stat.S_IRGRP
+        | stat.S_IWGRP
+        | stat.S_IROTH
+        | stat.S_IWOTH,
+    )
+    g.show(ofile)
+
 
 #############################################################################
 def Scafnet2Html(scafnet, scafname, scratchdir, ofile):
-  logging.debug(f"pyvis.network.Network()...")
-  g = pyvis.network.Network(notebook=False, height='800px', width='1000px', heading=scafname)
-  logging.debug(f"pyvis.network.Network()... Done.")
+    logging.debug(f"pyvis.network.Network()...")
+    g = pyvis.network.Network(
+        notebook=False, height="800px", width="1000px", heading=scafname
+    )
+    logging.debug(f"pyvis.network.Network()... Done.")
 
-  for i,n in enumerate(scafnet.nodes):
-    logging.debug(f"{i+1}. util.moltosvg(rdkit.Chem.MolFromSmiles({n})...")
-    svg = util.moltosvg(rdkit.Chem.MolFromSmiles(n))
-    with open(f'{scratchdir}/{i}.svg', 'w') as outf:
-      outf.write(svg)
-    logging.debug(f"g.add_node()...")
-    g.add_node(i, shape="image", label=' ', image=f'{scratchdir}/{i}.svg', title=svg, size=60)
-    # Segmentation fault after last node.
-  logging.debug(f"util.moltosvg()... Done.")
-  for i,e in enumerate(scafnet.edges):
-    logging.debug(f"{i+1}. g.add_edge()...")
-    g.add_edge(e.beginIdx, e.endIdx, label=str(e.type))
-  logging.debug(f"g.add_edge()... Done.")
+    for i, n in enumerate(scafnet.nodes):
+        logging.debug(f"{i+1}. util.moltosvg(rdkit.Chem.MolFromSmiles({n})...")
+        svg = util.moltosvg(rdkit.Chem.MolFromSmiles(n))
+        with open(f"{scratchdir}/{i}.svg", "w") as outf:
+            outf.write(svg)
+        logging.debug(f"g.add_node()...")
+        g.add_node(
+            i,
+            shape="image",
+            label=" ",
+            image=f"{scratchdir}/{i}.svg",
+            title=svg,
+            size=60,
+        )
+        # Segmentation fault after last node.
+    logging.debug(f"util.moltosvg()... Done.")
+    for i, e in enumerate(scafnet.edges):
+        logging.debug(f"{i+1}. g.add_edge()...")
+        g.add_edge(e.beginIdx, e.endIdx, label=str(e.type))
+    logging.debug(f"g.add_edge()... Done.")
 
-  VisJS_options = {
-    "edges": {
-     "font":{
-     "size":20
-     }
-    },
-    "nodes": {
-      "font": {
-        "color": "rgba(214,47,66,1)",
-        "size": 16,
-        "face": "tahoma"
-      }
-    },
-    "physics": {
-      "forceAtlas2Based": {
-        "gravitationalConstant": -120,
-        "springLength": 200,
-        "avoidOverlap": 0.42
-      },
-      "minVelocity": 0.75,
-      "solver": "forceAtlas2Based"
+    VisJS_options = {
+        "edges": {"font": {"size": 20}},
+        "nodes": {"font": {"color": "rgba(214,47,66,1)", "size": 16, "face": "tahoma"}},
+        "physics": {
+            "forceAtlas2Based": {
+                "gravitationalConstant": -120,
+                "springLength": 200,
+                "avoidOverlap": 0.42,
+            },
+            "minVelocity": 0.75,
+            "solver": "forceAtlas2Based",
+        },
     }
-  }
-  logging.debug(f"g.set_options()...")
-  g.set_options(options=json.dumps(VisJS_options))
-  #g.show_buttons()
-  if ofile:
-    logging.info(f"Writing SCAFNET HTML to: {ofile}")
-    g.save_graph(ofile)
-  return g
+    logging.debug(f"g.set_options()...")
+    g.set_options(options=json.dumps(VisJS_options))
+    # g.show_buttons()
+    if ofile:
+        logging.info(f"Writing SCAFNET HTML to: {ofile}")
+        g.save_graph(ofile)
+    return g
+
 
 #############################################################################
